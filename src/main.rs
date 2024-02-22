f845abe3fbdb9cd2447ab6ac7351bef35180bb6f
Ivan <blaskovic.ivan1@gmail.com>
2024-02-22 13:13:16 +0100

    Dodana struktura za veze

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,18 @@ use tokio::prelude::*;
 use std::thread;
 
 use eframe::egui;
-use egui::Color32;
+use egui::{Color32, Stroke};
 
 fn main() -> Result<(), eframe::Error> {
     thread::spawn(move || tcp_connections());
+
+    let node1 = Node{center: egui::pos2(250.0, 100.0), radius: 10.0, color: Color32::WHITE};
+    let node2 = Node{center: egui::pos2(400.0, 100.0), radius: 10.0, color: Color32::WHITE};
     
     unsafe {
         NODES.push(Node{center: egui::pos2(250.0, 100.0), radius: 10.0, color: Color32::WHITE});
         NODES.push(Node{center: egui::pos2(400.0, 100.0), radius: 10.0, color: Color32::WHITE});
+        LINKS.push(Link{node1, node2});
     };
 
 
@@ -25,7 +29,7 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
     eframe::run_native(
-        "Simulator",
+        "Vizualizator",
         options,
         Box::new(|_cc| {
             Box::<MyApp>::default()
@@ -45,7 +49,12 @@ static mut NODES: Vec<Node> = Vec::new();
 
 
 
+struct Link {
+    node1: Node,
+    node2: Node,
+}
 
+static mut LINKS: Vec<Link> = Vec::new();
 
 
 
@@ -54,8 +63,7 @@ struct MyApp {
 
 impl Default for MyApp {
     fn default() -> Self {
-        Self { 
-        }
+        Self {}
     }
 }
 
@@ -77,6 +85,11 @@ impl eframe::App for MyApp {
             for node in nodes {
                 ui.painter().circle_filled(node.center, node.radius, node.color);
             }
+
+            let links = unsafe { &LINKS };
+            for link in links {
+                ui.painter().line_segment([link.node1.center, link.node2.center], Stroke::new(1.0, Color32::WHITE));
+            }
         });
     }
 }
@@ -86,23 +99,17 @@ impl eframe::App for MyApp {
 fn tcp_connections() {
     let addr = "127.0.0.1:8020".parse().unwrap();
     let socket = TcpListener::bind(&addr).unwrap();
-    println!("Listening on: {}", addr);
 
-     // Start the Tokio runtime
-     tokio::run(socket.incoming().for_each(|stream| {
-        // Handle each socket concurrently.
+    tokio::run(socket.incoming().for_each(|stream| {
         tokio::spawn(handle_connection(stream));
         Ok(())
-    }).map_err(|e| eprintln!("Error in main: {}", e)));
+    }).map_err(|e| eprintln!("Error in tcp_connections: {}", e)));
 
 }
 
 
 
-fn handle_connection(stream: TcpStream) -> Box<dyn Future<Item = (), Error = ()> + Send> {
-    let peer_addr = stream.peer_addr().expect("Unable to get peer address");
-    println!("Accepted connection from: {}", peer_addr);
-    
+fn handle_connection(stream: TcpStream) -> Box<dyn Future<Item = (), Error = ()> + Send> {    
     let handle = tokio::io::read_to_end(stream, Vec::new())
         .and_then(move |(_, data)| {
             let received_data_str = String::from_utf8_lossy(&data);
