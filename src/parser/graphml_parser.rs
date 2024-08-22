dd0e37e2dac6ca83b6dc5d9a128325e7946e8ab7
Ivan <blaskovic.ivan1@gmail.com>
2024-08-22 10:04:16 +0200

    Parser - default node pos

--- src/parser/graphml_parser.rs
+++ src/parser/graphml_parser.rs
@@ -1,10 +1,14 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::BufReader;
 
 use xml::reader::{EventReader, XmlEvent};
 
 use crate::{Link, MyApp, Node};
+
+fn scale_float_to_int(value: f32) -> i32 {
+    (value * 1000.0).round() as i32
+}
 
 pub fn parse_graphml(app: &MyApp, nodes_arc: &mut Vec<Node>, links_arc: &mut Vec<Link>, reader: BufReader<File>) {
     let parser = EventReader::new(reader);
@@ -19,6 +23,9 @@ pub fn parse_graphml(app: &MyApp, nodes_arc: &mut Vec<Node>, links_arc: &mut Vec
     let mut current_source = String::new();
     let mut current_target = String::new();
     let mut id_map = HashMap::new();
+    let mut edges: Vec<(String, String)> = Vec::new();
+    let mut nodes_without_pos: Vec<String> = Vec::new();
+    let mut occupied_positions: HashSet<(i32, i32)> = HashSet::new();
 
     for event in parser {
         match event {
@@ -47,6 +54,7 @@ pub fn parse_graphml(app: &MyApp, nodes_arc: &mut Vec<Node>, links_arc: &mut Vec
                                 _ => {}
                             }
                         }
+                        edges.push((current_source.clone(), current_target.clone()));
                         let link_data = Link {
                             node1_id: current_source.clone(),
                             node2_id: current_target.clone(),
@@ -76,11 +84,74 @@ pub fn parse_graphml(app: &MyApp, nodes_arc: &mut Vec<Node>, links_arc: &mut Vec
                     new_node.radius = app.node_default_radius;
                     new_node.color = egui::Color32::WHITE;
 
+                    if new_node.center.x == 0.0 && new_node.center.y == 0.0 {
+                        nodes_without_pos.push(new_node.id.clone());
+                    } else {
+                        occupied_positions.insert((scale_float_to_int(new_node.center.x), scale_float_to_int(new_node.center.y)));
+                    }
+
                     nodes_arc.push(new_node.clone());
                     id_map.insert(new_node.id.clone(), nodes_arc.len() - 1);
                 }
             }
             _ => {}
         }
+    }
+
+
+    
+    // Čvorovi bez zadane pozicije
+    let mut new_positions: Vec<(usize, f32, f32)> = Vec::new();
+
+    for node_id in nodes_without_pos {
+        if let Some(node_index) = id_map.get(&node_id) {
+            let connected_nodes: Vec<_> = edges.iter()
+                .filter(|(source, target)| source == &node_id || target == &node_id)
+                .map(|(source, target)| {
+                    if source == &node_id { target } else { source }
+                })
+                .collect();
+
+            let mut sum_x = 0.0;
+            let mut sum_y = 0.0;
+            let mut count = 0;
+
+            for connected_node_id in connected_nodes {
+                if let Some(connected_node_index) = id_map.get(connected_node_id) {
+                    let connected_node = &nodes_arc[*connected_node_index];
+                    if connected_node.center.x != 0.0 || connected_node.center.y != 0.0 {
+                        sum_x += connected_node.center.x;
+                        sum_y += connected_node.center.y;
+                        count += 1;
+                    }
+                }
+            }
+
+            let mut pos_x;
+            let pos_y;
+
+            if count > 0 {
+                pos_x = sum_x / count as f32;
+                pos_y = sum_y / count as f32;
+            } else {
+                // ako svi susjedni čvorovi nemaju zadanu poziciju, postavi default: (400, 200)
+                pos_x = 400.0;
+                pos_y = 200.0;
+            }
+
+            // ako već postoji čvor na toj poziciji, pomakni novi desno
+            while occupied_positions.contains(&(scale_float_to_int(pos_x), scale_float_to_int(pos_y))) {
+                pos_x += 35.0;
+            }
+            occupied_positions.insert((scale_float_to_int(pos_x), scale_float_to_int(pos_y)));
+
+            new_positions.push((*node_index, pos_x, pos_y));
+        }
+    }
+
+    for (node_index, pos_x, pos_y) in new_positions {
+        let node = &mut nodes_arc[node_index];
+        node.center.x = pos_x;
+        node.center.y = pos_y;
     }
 }
